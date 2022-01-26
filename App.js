fc51095c06bce9a0443b1c18bb2f26554bae0a44
Moabe <moabeluis2018@gmail.com>
2022-01-25 21:57:36 -0300

    importações aula 01

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigation } from "@react-navigation/stack";
+
+import Contacts from "./src/pages/Contacts"
+import Information from "./src/pages/Information"
+
+export default function App(){
+  return(
+    
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
